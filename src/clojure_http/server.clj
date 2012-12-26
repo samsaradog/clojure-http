2f686efb632bf1e43dffcec9ddd29ac919a4cc45
Shinkyo Will Warner <gwmf7rru35@snkmail.com>
2012-12-26 10:58:15 -0500

    refactor for rack interface

--- src/clojure_http/server.clj
+++ src/clojure_http/server.clj
@@ -13,18 +13,24 @@
 (defn create-socket [port]
 	(new ServerSocket port))
 	
+(defn accept-connection [server-socket]
+	(. server-socket accept))
+	
+(defn socket-closed? [server-socket]
+	(. server-socket isClosed))
+	
 (defn launch-connector [socket connector root]
 	(let [input  (create-reader socket)
           output (create-writer socket)]
 		(connector input output root)))
 
 (defn connect-input-and-output [server-socket connector root]
-    (let [socket (. server-socket accept)]
+    (let [socket (accept-connection server-socket)]
     	(.start (Thread. #(launch-connector socket connector root)))))
 
 (defn start-server [port connector root]
     (let [server-socket (create-socket port)]
-        (while (not (. server-socket isClosed))
+        (while (not (socket-closed? server-socket))
            (try
               (connect-input-and-output server-socket connector root)
             (catch SocketException e)))))
